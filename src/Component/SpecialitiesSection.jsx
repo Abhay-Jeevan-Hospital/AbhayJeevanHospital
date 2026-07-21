7d532af63c075f1792c6ada8b7898bbdcdcea280
Salfartech <Salfartechmedia@gmail.com>
2026-07-21 20:25:05 +0530

    Revert "Reapply "gg""
    This reverts commit 17057a517a03eb168bde556cc327d1965e38f5b3.

--- src/Component/SpecialitiesSection.jsx
+++ src/Component/SpecialitiesSection.jsx
@@ -289,6 +289,7 @@ export default function SpecialtiesSection() {
 
                                     </div>
                                 </Reveal>
+
                                 {/* Buttons */}
                                 <Reveal>
 
